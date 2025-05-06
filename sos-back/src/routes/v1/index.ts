9acac3767bb1bf67210b00b5fe62232653067343
Franco Peratta <franco.peratta@hotmail.com>
2025-05-06 14:30:40 -0300

    Remove twilio

--- sos-back/src/routes/v1/index.ts
+++ sos-back/src/routes/v1/index.ts
@@ -5,7 +5,6 @@ import patientRouter from "./patient"
 import providerRouter from "./provider"
 import userRouter from "./user"
 import authRouter from "./auth"
-import videocallRouter from "./videocall"
 import { auth } from "../../middlewares/auth"
 
 const router = Router()
@@ -18,6 +17,5 @@ router.use("/patient", auth, patientRouter)
 router.use("/provider", auth, providerRouter)
 router.use("/appointment", auth, appointmentRouter)
 router.use("/user", auth, userRouter)
-router.use("/videocall", auth, videocallRouter)
 
 export default router
--- sos-back/src/routes/v1/videocall/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import twilioRouter from "./twilio/twilio"
-
-const router = express.Router()
-
-router.use("/twilio", twilioRouter)
-
-export default router
